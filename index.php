22bdcd0b3080a125ff763d97caff0bce82b8ca73
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-09-15 09:32:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
-<br><br><h2 class="center">HOLA DESDE EL SISTEMA SIC EN LA WEB</h2>
-<br><br><h2 class="center">Inicia en 3....</h2>
+<br><br><h1 align="center">HOLA DESDE EL SISTEMA SIC EN LA WEB</h1>
+<br><br><h2 align="center">Iniciando ....</h2>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<h3 align="right">Versión 5.0 </h3>
