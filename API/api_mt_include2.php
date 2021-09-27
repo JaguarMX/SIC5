2edd628caf5e89d8441ff3e5bfe2c2b6ea239b47
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-09-27 10:49:35 -0500

    Update api_mt_include2.php

--- API/api_mt_include2.php
+++ API/api_mt_include2.php
@@ -20,10 +20,10 @@ class routeros_api
     var $debug     = false; //  Show debug information
     var $connected = false; //  Connection state
      //  Port to connect to (default 8729 for ssl)
-    var $ssl       = false; //  Connect using SSL (must enable api-ssl in IP/Services)
-    var $timeout   = 3;     //  Connection attempt timeout and data read timeout
+    var $ssl       = true; //  Connect using SSL (must enable api-ssl in IP/Services)
+    var $timeout   = 4;     //  Connection attempt timeout and data read timeout
     var $attempts  = 5;     //  Connection attempt count
-    var $delay     = 3;     //  Delay between connection attempts in seconds
+    var $delay     = 4;     //  Delay between connection attempts in seconds
     var $socket;            //  Variable for storing socket resource
     var $error_no;          //  Variable for storing connection error number, if any
     var $error_str;         //  Variable for storing connection error text, if any
