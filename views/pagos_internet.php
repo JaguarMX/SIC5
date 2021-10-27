656356ef44401c67ec40c169c70b5b67e7f1b270
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2021-10-27 09:54:11 -0500

    pagos bancos fernando
    pagos bancos fernando

--- views/pagos_internet.php
+++ views/pagos_internet.php
@@ -314,7 +314,7 @@ $area = mysqli_fetch_array(mysqli_query($conn, "SELECT area FROM users WHERE use
             <label for="resto">Calcular días restantes</label>
           </p>
         </div>
-        <?php if ($user_id == 88 OR $user_id == 38 OR $user_id == 10 OR $user_id == 77 OR $user_id == 49 OR $user_id == 70) { 
+        <?php if (($area['area']=="Administrador" AND (in_array($id_user, array(10, 70, 49, 88, 38, 84))))) { 
           $Ser = '';
         }else{ $Ser = 'disabled="disabled"';}?>
         <div class="col s6 m1 l1">
