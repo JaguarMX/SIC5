6151660ea9c73925f228057ecae88252acff9df2
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2022-01-13 11:34:43 -0600

    Update ir_consumo.php

--- php/ir_consumo.php
+++ php/ir_consumo.php
@@ -12,19 +12,18 @@ $comunidad = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM comunidades W
 //SACAMOS LA INFO DEL SERVIDOR
 $id_servidor = $comunidad['servidor'];
 $serv = mysqli_fetch_array(mysqli_query($conn,"SELECT * FROM servidores WHERE id_servidor = $id_servidor"));
-
 //////// INFORMACION DEL SERVIDOR
 $ServerList = $serv['ip_local'] ; //ip_de_tu_API
 $Port = $serv['port_web']; //puerto_API
 
-$URL =$ServerList.':'.$Port.'/graphs/';
+$URL = $ServerList.':'.$Port.'/graphs/';
 
 echo '<script>M.toast({html:"Esto puede tardar algunos segundos.", classes: "rounded"})</script>';
 ?>
 <script>
 	var a = document.createElement("a");
 		a.target = "_blank";
-		a.href = "http://'.$URL.'";
+		a.href = "http://<?php echo $URL; ?>";
 		a.click();
 </script>
 
