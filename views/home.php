892937edf80aa6f463e635b9647f3501bcf1a863
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2022-03-14 13:25:51 -0600

    msj Bienvenida

--- views/home.php
+++ views/home.php
@@ -8,6 +8,12 @@
 <title>SIC | Inicio</title>
 </head>
 <main>
+<script>
+	$(document).ready(function(){
+	    $('#bienvenida').modal();
+	    $('#bienvenida').modal('open'); 
+	 });
+</script>
 <body>
 	<div class="row">
 		 <img class="materialboxed" width="100%" src="../img/home.jpg">
@@ -15,6 +21,33 @@
  	<div class="row container">
  		<h4 class="center-align">Servicios Integrales de Computación</h4>
  	</div>
+ 	<!--Modal cortes BIENVENIDA-->
+	<div id="bienvenida" class="modal">
+	  <div class="modal-content">
+	    <h3 class="red-text center">¡ Bienvenid@ !</h3><br>
+	    <?php
+	    $id = $_SESSION['user_id'];
+	    if ($id == 88) {
+	    	$Usuario = 'Elvira';
+	    	$MSJ = 'Usa el sistema correctamente o puedes tener una sanción y cancelación de cuenta (te estamos vigilando). ;) ';
+	    }else{
+	    	$Usuario = $_SESSION['user_name'];
+	    	$MSJ = 'SIC te invita a hacer un buen uso del sistema, que tengas un dia muy productivo. :)';
+	    }
+	    if($id){
+
+	    }
+	    ?>
+	    <h4 class="blue-text center"><b><?php echo $Usuario ?></b></h4><br>
+	   
+	    <h5><b>"<?php echo $MSJ ?>"</b></h5>
+	     
+	  </div>
+	  <div class="modal-footer">
+	      <a href="#" class="modal-action modal-close waves-effect waves-red btn-flat">Cerrar<i class="material-icons right">close</i></a>
+	  </div>
+	</div>
+ 	<!--Modal cortes BIENVENIDA-->
 </body>
 </main>
 </html>
