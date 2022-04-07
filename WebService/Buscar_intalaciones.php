37336f330ec087672556f6b030f023945658def9
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2022-04-07 13:23:52 -0500

    Update Buscar_intalaciones.php

--- WebService/Buscar_intalaciones.php
+++ WebService/Buscar_intalaciones.php
@@ -5,22 +5,20 @@ include('../php/conexion.php');
 $codigo=$_GET['codigo'];;// RECIBIMOS EL ID DE LA RUTA POR GET
 
 #CONSULTAMOS TODAS LAS INSTALACIONES QUE ALLA DE ESTA RUTA
-$resultado = $conn->query("SELECT * FROM tmp_pendientes WHERE ruta_inst =$codigo");
+$resultado = $conn->query("SELECT * FROM tmp_pendientes INNER JOIN clientes ON tmp_pendientes.id_cliente = clientes.id_cliente WHERE ruta_inst =$codigo");
 
 $arr = array();//CREAMOS UN ARRAY VACIO PARA COLOCAR LA INFORAMCION NECESARIA
 #RECORREMOS CADA INSTALACION CON UN CICLO Y LO VACIAMOS EN UN ARRAY 
 while($cliente=$resultado -> fetch_array()){	
-    $id_cliente = $cliente['id_cliente'];
-    $sql_cliente = mysqli_fetch_array(mysqli_query($conn,"SELECT * FROM clientes WHERE id_cliente = $id_cliente"));
     $id_comunidad = $cliente['lugar'];
     $sql_comunidad = mysqli_fetch_array(mysqli_query($conn,"SELECT nombre FROM comunidades WHERE id_comunidad='$id_comunidad'"));
     $id_paquete = $cliente['paquete'];
-    $paquete = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM paquetes WHERE id_paquete=$id_paquete"));
+    $paquete = mysqli_fetch_array(mysqli_query($conn, "SELECT subida, bajada, mensualidad FROM paquetes WHERE id_paquete=$id_paquete"));
     $Apagar = $cliente['total']-$cliente['dejo'];
 	#LLEMANMOS NUESTRO ARRAY POR CADA REPORTE ENCONTRADO
-	$arr['id_cliente'] =$id_cliente;
+	$arr['id_cliente'] =$cliente['id_cliente'];
 	$arr['nombre'] =$cliente['nombre'];
-	$arr['servicio'] =$sql_cliente['servicio'];
+	$arr['servicio'] =$cliente['servicio'];
 	$arr['telefono'] =$cliente['telefono'];
 	$arr['comunidad'] =$sql_comunidad['nombre'];
 	$arr['referencia'] =$cliente['referencia'];
