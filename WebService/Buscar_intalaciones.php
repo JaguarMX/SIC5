5e39d000a8b5fdaa66f248ee1a7db00c3d527486
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2022-04-07 12:16:51 -0500

    Update Buscar_intalaciones.php

--- WebService/Buscar_intalaciones.php
+++ WebService/Buscar_intalaciones.php
@@ -7,7 +7,7 @@ $codigo=$_GET['codigo'];;// RECIBIMOS EL ID DE LA RUTA POR GET
 #CONSULTAMOS TODAS LAS INSTALACIONES QUE ALLA DE ESTA RUTA
 $resultado = $conn->query("SELECT * FROM tmp_pendientes INNER JOIN clientes ON tmp_pendientes.id_cliente = clientes.id_cliente WHERE ruta_inst =$codigo");
 
-$arr = array();//CREAMOS UN ARRAY VACIO PARA COLOCAR LA INFORAMCION NECESARIA
+$array = array();//CREAMOS UN ARRAY VACIO PARA COLOCAR LA INFORAMCION NECESARIA
 #RECORREMOS CADA INSTALACION CON UN CICLO Y LO VACIAMOS EN UN ARRAY 
 while($cliente=$resultado -> fetch_array()){	
     $id_comunidad = $cliente['lugar'];
@@ -16,20 +16,20 @@ while($cliente=$resultado -> fetch_array()){
     $paquete = mysqli_fetch_array(mysqli_query($conn, "SELECT subida, bajada, mensualidad FROM paquetes WHERE id_paquete=$id_paquete"));
     $Apagar = $cliente['total']-$cliente['dejo'];
 	#LLEMANMOS NUESTRO ARRAY POR CADA REPORTE ENCONTRADO
-	$arr['id_cliente'] =$cliente['id_cliente'];
-	$arr['nombre'] =$cliente['nombre'];
-	$arr['servicio'] =$cliente['servicio'];
-	$arr['telefono'] =$cliente['telefono'];
-	$arr['comunidad'] =$sql_comunidad['nombre'];
-	$arr['referencia'] =$cliente['referencia'];
-	$arr['total'] =$cliente['total'];
-	$arr['dejo'] =$cliente['dejo'];
-	$arr['Apagar'] =$Apagar;
-	$arr['paquete'] ='(Subida/Bajada)'.$paquete['subida']."/".$paquete['bajada'];
-	$arr['fecha'] =$cliente['fecha_registro'];
+	$array['id_cliente'] =$cliente['id_cliente'];
+	$array['nombre'] =$cliente['nombre'];
+	$array['servicio'] =$cliente['servicio'];
+	$array['telefono'] =$cliente['telefono'];
+	$array['comunidad'] =$sql_comunidad['nombre'];
+	$array['referencia'] =$cliente['referencia'];
+	$array['total'] =$cliente['total'];
+	$array['dejo'] =$cliente['dejo'];
+	$array['Apagar'] =$Apagar;
+	$array['paquete'] ='(Subida/Bajada)'.$paquete['subida']."/".$paquete['bajada'];
+	$array['fecha'] =$cliente['fecha_registro'];
 
-    $producto[] = $arr;
+    $instalaciones[] = $array;
 }
 
-echo json_encode($producto, JSON_UNESCAPED_UNICODE);
+echo json_encode($instalaciones, JSON_UNESCAPED_UNICODE);
 ?>
