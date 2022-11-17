1f6f5d672e7d86401e3b41341e9bd4522c1050f4
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2022-11-17 14:10:52 -0600

    Update imprimir.php

--- php/imprimir.php
+++ php/imprimir.php
@@ -1,135 +1,205 @@
 <?php
-include('../php/conexion.php');
-$id_pago = $_GET['IdPago'];
+#INCLUIMOS EL ARCHIVO CON LA CONEXION A LA BASE DE DATPS
+    include('../php/conexion.php');
+    #INCLUIMOS EL ARCHIVO CON LAS LIBRERIAS DE FPDF PARA PODER CREAR ARCHIVOS CON FORMATO PDF
+    include("../fpdf/fpdf.php");
+    #INCLUIMOS EL PHP DONDE VIENE LA INFORMACION DEL INICIO DE SESSION
+    include('is_logged.php');
 
-//Incluimos la libreria fpdf
-include("../fpdf/fpdf.php");
-include("is_logged.php");
+    $id_pago = $_GET['IdPago'];;//TOMAMOS EL ID DEl PAGO
+    $pago = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM pagos WHERE id_pago = $id_pago"));
 
+    #DEFINIMOS UNA ZONA HORARIA
+    date_default_timezone_set('America/Mexico_City');
+    $Fecha_hoy = date('Y-m-d');//CREAMOS UNA FECHA DEL DIA EN CURSO SEGUN LA ZONA HORARIA
+   
 class PDF extends FPDF{
-	function folioCliente(){
-		global $id_pago;
-		global $conn;
 
-		$fila = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM pagos WHERE id_pago = $id_pago"));
-		$id_user = $fila['id_user'];
-		$user = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM users WHERE user_id=$id_user"));
-		$id_cliente = $fila['id_cliente'];
-		$tipo_pago = $fila['tipo'];
-        if ((mysqli_num_rows(mysqli_query($conn, "SELECT * FROM clientes WHERE id_cliente = $id_cliente"))) == 0) {
-            $cliente = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM especiales WHERE id_cliente = $id_cliente"));
-        }else{
-            $cliente = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM clientes WHERE id_cliente = $id_cliente"));
-        }
-		 // Colores de los bordes, fondo y texto
-            $this->SetFillColor(255,255,255);
-            $this->SetTextColor(0,0,0);
-            $this->AddPage();
-            $this->Image('../img/logo_ticket.jpg',28,4,20);
-            $this->SetFont('Arial','B',13);
-            $this->SetY(30);
-            $this->Cell(90,4,'Fecha: '.$fila['fecha'],0,0,'C',true);
-            $this->SetFont('Arial','B',10);
-            $this->Ln(5);
-            $this->Cell(20,4,utf8_decode('Folio: '.$id_pago),0,0,'L',true);
-            $this->SetFont('Arial','',10);
-            $this->Ln(5);
-            $this->Cell(20,4,utf8_decode('No. Cliente: '.$fila['id_cliente']),0,0,'L',true);
-            $this->Ln(5);
-            $this->MultiCell(60,4,utf8_decode('Nombre:'.$cliente['nombre']),0,'L',true);
-            $this->Ln(1);
-            $this->MultiCell(60,4,utf8_decode('Descripción: '.$fila['descripcion']),0,'L',true);
-            $this->Ln(1);
-            $this->MultiCell(60,4,utf8_decode('Tipo de Cambio: '.$fila['tipo_cambio']),0,'L',true);
-            $this->Ln(1);
-            if ($fila['tipo_cambio'] == 'Banco' OR $fila['tipo_cambio'] == 'SAN') {
-                $sqlR = mysqli_query($conn, "SELECT * FROM referencias WHERE id_pago = $id_pago");
-                if (mysqli_num_rows($sqlR) == 0) {
-                  $refe = "Sin";
-                }else{
-                  $referecia = mysqli_fetch_array($sqlR);
-                  $refe = $referecia['descripcion'];
-                }            
-                $this->MultiCell(60,4,utf8_decode('Referencia: '.$refe),0,'L',true);
-                $this->Ln(1);
-            }
-            if ((in_array($id_user, array(47, 42, 31, 52, 67, 57, 63, 24, 55, 29, 64, 93, 99))) AND $tipo_pago != 'Otros Pagos') {
-                $this->Cell(20,4,utf8_decode($fila['tipo'].': $'.$fila['cantidad'].'.00'),0,0,'L',true);
-                $this->Ln(5);
-                $this->Cell(20,4,utf8_decode('Comisión: + $10.00'),0,0,'L',true); 
-                $this->Ln(5);
-                $Total = $fila['cantidad']+10;
-                $this->Cell(20,4,utf8_decode(' TOTAL:  $'.$Total.'.00'),0,0,'L',true);    
-            }else{
-                $this->Cell(20,4,utf8_decode('Cantidad:  $'.$fila['cantidad'].'.00'),0,0,'L',true);
-                $this->Ln(5);                
-                $this->Cell(20,4,utf8_decode(' TOTAL:  $'.$fila['cantidad'].'.00'),0,0,'L',true);  
-                $this->Ln(5);                 
-                $this->Cell(20,4,utf8_decode('Tipo: '.$fila['tipo']),0,0,'L',true);
-            }
-            $this->Ln(5);
-            if ($tipo_pago == 'Abono' AND $fila['descripcion'] != 'Abono de instalacion') {
-                // SACAMOS LA SUMA DE TODAS LAS DEUDAS Y ABONOS ....
-                $deuda = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS suma FROM deudas WHERE id_cliente = $id_cliente"));
-                $abono = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS suma FROM pagos WHERE id_cliente = $id_cliente AND tipo = 'Abono'"));
-                  //COMPARAMOS PARA VER SI LOS VALORES ESTAN VACIOS::
-                if ($deuda['suma'] == "") {
-                    $deuda['suma'] = 0;
-                }elseif ($abono['suma'] == "") {
-                    $abono['suma'] = 0;
-                }
-                //SE RESTAN DEUDAS DE ABONOS 
-                $Saldo = $abono['suma']-$deuda['suma'];
-                $msj = 'Saldo a Favor: $';
-                if ($Saldo < 0) {
-                    $Saldo = $Saldo*(-1);
-                    $msj = 'Saldo a Pagar: $';
-                }
-
-                $this->Cell(20,4,utf8_decode($msj.$Saldo.'.00'),0,0,'L',true);
-                $this->Ln(5);
-            }
-            $this->MultiCell(60,4,utf8_decode('Atendió: '.$user['firstname'].' '.$user['lastname']),0,'L',true);
-            
-            if ($fila['tipo_cambio'] == 'Credito') {
-                $this->Ln(5);
-                $this->MultiCell(70,4,utf8_decode('_________________________________'),0,'L',false);
-                $this->SetX(6);
-                $this->MultiCell(70,7,utf8_decode('Nombre y Firma (Cliente)'),0,'C',false);
-                $this->Ln(1);
-            }
-            if ($fila['fecha'] < date('Y-m-d')) {
-                $this->SetFont('Arial','',8);
-                $this->Ln(2);
-                $this->MultiCell(60,4,utf8_decode('Fecha Reimprieción: '.date('Y-m-d')),0,'L',true);
-                
-            }
-            $this->Ln(2);
-            $this->SetFont('Arial','B',9);
-            if ($tipo_pago == 'Mensualidad' ){
-                $this->MultiCell(60,7,utf8_decode('GRACIAS. RECORDARLE QUE SU PRÓXIMO PAGO DEBERÁ SER ANTES DEL: '.$cliente['fecha_corte'].
-                    '
-RECOMENDACIONES:
-1.- Contar con línea regulada "regulador de corriente".
-2.- No modificar orden del cableado.
-3.- No presionar botónes de equipos.
-4.- En caso de falla comunicarse al 433 935 62 86.'),1,'C',true);
-            }else if ($tipo_pago == 'Reporte' OR $tipo_pago == 'Liquidacion') {
-                $this->MultiCell(60,7,utf8_decode('GRACIAS POR SU PAGO.
-RECOMENDACIONES:
-1.- Contar con línea regulada "regulador de corriente".
-2.- No modificar orden del cableado.
-3.- No presionar botónes de equipos.
-4.- En caso de falla comunicarse al 433 935 62 86.'),1,'C',true);
-            }else{
-                $this->MultiCell(60,7,utf8_decode('GRACIAS POR SU PAGO; MÁS QUE TECNOLOGÍA SOMOS COMUNICACIÓN.'),1,'C',true);
-            }
-            mysqli_close($conn);
-        }
     }
 
     $pdf = new PDF('P', 'mm', array(80,297));
-    $pdf->SetTitle('Pago');
-    $pdf->folioCliente();
-    $pdf->Output('Pago','I');
+    $pdf->setTitle(utf8_decode('SIC | TICKET PAGO'));// TITULO BARRA NAVEGACION
+    $pdf->AddPage();
+
+    $pdf->Image('../img/logo.jpg', 30, 2, 20, 21, 'jpg'); /// LOGO SIC
+
+    /// INFORMACION DE LA EMPRESA ////
+    $pdf->SetFont('Courier','B', 8);
+    $pdf->SetY($pdf->GetY()+15);
+    $pdf->SetX(5);
+    $pdf->MultiCell(70,3,utf8_decode('SERVICIOS INTEGRALES DE COMPUTACIÓN'."\n".'GABRIEL VALLES REYES'."\n".'RFC: VARG7511217E5'."\n".'AV. HIDALGO COL. CENTRO C.P. 99100 SOMBRERETE, ZACATECAS '."\n".'TEL. 4339356288'),0,'C',0);
+    if ($pago['fecha'] < $Fecha_hoy) {
+        $pdf->SetY($pdf->GetY()+3);
+        $pdf->SetX(5);
+        $pdf->MultiCell(70,3,utf8_decode('Fecha Reimprieción: '.date_format(new \DateTime($Fecha_hoy), "d/m/Y" )),0,'R',0);
+    }
+    /// INFORMACION DEL PAGO
+    $pdf->SetY($pdf->GetY()+4);
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','B', 10);    
+    $folio = substr(str_repeat(0, 5).$id_pago, - 6);
+    $pdf->MultiCell(70,4,utf8_decode(date_format(new \DateTime($pago['fecha'].' '.$pago['hora']), "d/m/Y H:i" ).'             FOLIO: '.$folio),0,'C',0);
+
+    $pdf->SetY($pdf->GetY()+1);
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','', 8);
+    $pdf->MultiCell(70,3,utf8_decode('------------------------------------------------------------------------'),0,'L',0);
+
+    $pdf->SetY($pdf->GetY());
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','B', 11);
+    $pdf->MultiCell(70,4,utf8_decode('TICKET PAGO'),0,'C',0);
+
+    $pdf->SetY($pdf->GetY());
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','', 8);
+    $pdf->MultiCell(70,3,utf8_decode('------------------------------------------------------------------------'),0,'L',0); 
+    /// INFORMACION DEL CLIENTE
+    $id_cliente = $pago['id_cliente'];
+    if ((mysqli_num_rows(mysqli_query($conn, "SELECT * FROM clientes WHERE id_cliente = $id_cliente"))) == 0) {
+        $cliente = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM especiales WHERE id_cliente = $id_cliente"));
+    }else{
+        $cliente = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM clientes WHERE id_cliente = $id_cliente"));
+    }     
+    $pdf->SetY($pdf->GetY()+2);
+    $pdf->SetX(5);
+    $pdf->SetFont('Courier','B', 9);
+    $pdf->MultiCell(70,4,utf8_decode('N° CLIENTE: '.$id_cliente."\n".'NOMBRE:  '.$cliente['nombre']."\n".'TELEFONO:  '.$cliente['telefono']),0,'L',0);
+
+    $pdf->SetY($pdf->GetY()+2);
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','', 8);
+    $pdf->MultiCell(70,3,utf8_decode('------------------------------------------------------------------------'),0,'L',0);
+    // INFORMACION DEL PAGO
+    $pdf->SetY($pdf->GetY());
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','B', 11);
+    $pdf->MultiCell(70,4,utf8_decode('TIPO: '.$pago['tipo']),0,'C',0);
+    $pdf->SetY($pdf->GetY());
+    $pdf->SetX(5);       
+    $pdf->SetFont('Helvetica','', 8);
+    $pdf->MultiCell(70,3,utf8_decode('------------------------------------------------------------------------'),0,'L',0);
+
+    $pdf->SetY($pdf->GetY());
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','B', 9);    
+    $pdf->MultiCell(70,4,utf8_decode(' DESCRIPCION             T.CAMBIO      TOTAL'),0,'L',0);
+
+    $pdf->SetY($pdf->GetY());
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','', 8);
+    $pdf->MultiCell(70,3,utf8_decode('------------------------------------------------------------------------'),0,'L',0);
+    
+    
+    $pdf->SetY($pdf->GetY()+1);
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','', 9);
+    $pdf->MultiCell(35,3,utf8_decode($pago['descripcion']),0,'L',0);
+    $pdf->SetY($pdf->GetY()-3);
+    $pdf->SetX(40);
+    $pdf->MultiCell(14,3,utf8_decode($pago['tipo_cambio']),0,'R',0);    
+    $pdf->SetY($pdf->GetY()-3);
+    $pdf->SetX(55);
+    $pdf->MultiCell(20,3,utf8_decode('$'.sprintf('%.2f',$pago['cantidad'])),0,'R',0);
+
+    $id_user = $pago['id_user'];// ID DEL USUARIO AL QUE SE LE APLICO EL CORTE
+    if ((in_array($id_user, array(47, 42, 31, 52, 67, 57, 63, 24, 55, 29, 64, 93, 99))) AND $pago['tipo'] != 'Otros Pagos') {
+        $pdf->SetY($pdf->GetY()+1);
+        $pdf->SetX(5);
+        $pdf->SetFont('Helvetica','', 9);
+        $pdf->MultiCell(35,3,utf8_decode('Comisión de cobrador'),0,'L',0);
+        $pdf->SetY($pdf->GetY()-3);
+        $pdf->SetX(40);
+        $pdf->MultiCell(14,3,utf8_decode($pago['tipo_cambio']),0,'R',0);    
+        $pdf->SetY($pdf->GetY()-3);
+        $pdf->SetX(55);
+        $pdf->MultiCell(20,3,utf8_decode('$'.sprintf('%.2f',10)),0,'R',0);
+        $pago['cantidad'] += 10;
+    }
+    
+    $pdf->SetY($pdf->GetY()+2);
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','', 8);
+    $pdf->MultiCell(70,3,utf8_decode('------------------------------------------------------------------------'),0,'L',0);
+   
+    if ($pago['tipo'] == 'Abono' AND $pago['descripcion'] != 'Abono de instalacion') {
+        // SACAMOS LA SUMA DE TODAS LAS DEUDAS Y ABONOS ....
+        $deuda = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS suma FROM deudas WHERE id_cliente = $id_cliente"));
+        $abono = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS suma FROM pagos WHERE id_cliente = $id_cliente AND tipo = 'Abono'"));
+        //COMPARAMOS PARA VER SI LOS VALORES ESTAN VACIOS::
+        if ($deuda['suma'] == "") {
+                    $deuda['suma'] = 0;
+        }elseif ($abono['suma'] == "") {
+                    $abono['suma'] = 0;
+        }
+        //SE RESTAN DEUDAS DE ABONOS 
+        $Saldo = $abono['suma']-$deuda['suma'];
+        $msj = 'SALDO A FAVOR: ';
+        if ($Saldo < 0) {
+            $Saldo = $Saldo*(-1);
+            $msj = 'SALDO A PAGAR: ';
+        }
+        $pdf->SetFont('Helvetica','', 9);
+        $pdf->SetY($pdf->GetY());
+        $pdf->SetX(5);
+        $pdf->MultiCell(30,4,utf8_decode('IVA:'."\n".'SUBTOTAL:'."\n".'TOTAL:'."\n"."\n".$msj),0,'R',0);    
+        $pdf->SetY($pdf->GetY()-20);
+        $pdf->SetX(35);
+        $pdf->MultiCell(40,4,utf8_decode('$'.sprintf('%.2f',$pago['cantidad']*0.16)."\n".'$'.sprintf('%.2f',$pago['cantidad']-($pago['cantidad']*0.16))."\n".'$'.sprintf('%.2f',$pago['cantidad'])."\n"."\n".'$'.sprintf('%.2f',$Saldo)),0,'R',0);
+    }else{
+        $pdf->SetFont('Helvetica','', 9);
+        $pdf->SetY($pdf->GetY());
+        $pdf->SetX(5);
+        $pdf->MultiCell(30,4,utf8_decode('IVA:'."\n".'SUBTOTAL:'."\n".'TOTAL:'),0,'R',0);    
+        $pdf->SetY($pdf->GetY()-12);
+        $pdf->SetX(35);
+        $pdf->MultiCell(40,4,utf8_decode('$'.sprintf('%.2f',$pago['cantidad']*0.16)."\n".'$'.sprintf('%.2f',$pago['cantidad']-($pago['cantidad']*0.16))."\n".'$'.sprintf('%.2f',$pago['cantidad'])),0,'R',0);
+    }
+
+    $pdf->SetY($pdf->GetY()+6);
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','', 8);
+    $pdf->MultiCell(70,3,utf8_decode('------------------------------------------------------------------------'),0,'L',0);
+    $pdf->SetY($pdf->GetY());
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','B', 10);      
+    #TOMAMOS LA INFORMACION DEL USUARIO QUE ESTA LOGEADO QUIEN HIZO LOS COBROS
+    $usuario = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM users WHERE user_id = $id_user"));  
+    $pdf->MultiCell(70,4,utf8_decode('LE ATENDIO: '.$usuario['firstname'].' '.$usuario['lastname']),0,'C',0);
+    $pdf->SetY($pdf->GetY());
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','', 8);
+    $pdf->MultiCell(70,3,utf8_decode('------------------------------------------------------------------------'),0,'L',0);
+
+    if ($pago['tipo_cambio'] == 'Credito') {
+        $pdf->SetY($pdf->GetY()+3);
+        $pdf->SetX(5);
+        $pdf->SetFont('Helvetica','', 10);
+        $pdf->MultiCell(70,5,utf8_decode("\n"."\n"."\n".'__________________________________'."\n".'Nombre y Firma (Cliente)'),1,'C',0);
+    }
+
+    $pdf->SetY($pdf->GetY()+2);
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','', 8);
+    $pdf->MultiCell(70,3,utf8_decode('------------------------------------------------------------------------'),0,'L',0);
+    $pdf->SetY($pdf->GetY()+2);
+    $pdf->SetX(5);    
+    $pdf->SetFont('Helvetica','B', 10);      
+    $pdf->MultiCell(70,4,utf8_decode('¡GRACIAS POR TU PAGO!'."\n".'TODO LO QUE QUIERES ESTA EN SIC'),0,'C',0);
+    $pdf->SetY($pdf->GetY()+2);
+    $pdf->SetFont('Helvetica','', 8);
+    $pdf->SetX(5);
+    $pdf->MultiCell(70,3,utf8_decode('------------------------------------------------------------------------'),0,'L',0);
+    $msj = '';
+    if ($pago['tipo'] == 'Mensualidad') {
+        $msj = 'RECOMENDACIONES:'."\n"."\n".'1. DEBE REALIZAR SU PROXIMO PAGO ANTES         DEL:  '.date_format(new \DateTime($cliente['fecha_corte']), "d/m/Y")."\n".'2. CONTAR CON LINEA REGULADA                              (REGULADOR DE CORRIENRE).'."\n".'3. NO MODIFICAR EL ORDEN DEL CABLEADO.'."\n".'4. NO PRESIONAR BOTONES DEL MODEM.'."\n".'5. EN CASO DE ALGUNA "FALLA"                                COMUNICARSE AL 433 935 6286.';
+    }elseif ($pago['tipo'] == 'Reporte' OR $pago['tipo'] == 'Liquidacion'){
+        $msj = 'RECOMENDACIONES:'."\n"."\n".'1. CONTAR CON LINEA REGULADA                              (REGULADOR DE CORRIENRE).'."\n".'2. NO MODIFICAR EL ORDEN DEL CABLEADO.'."\n".'3. NO PRESIONAR BOTONES DEL MODEM.'."\n".'4. EN CASO DE ALGUNA "FALLA"                                COMUNICARSE AL 433 935 6286.';
+    }
+    $pdf->SetY($pdf->GetY()+3);
+    $pdf->SetX(5);
+    $pdf->SetFont('Helvetica','B', 8);
+    $pdf->MultiCell(70,5,utf8_decode($msj),1,'L',0);
+
+    $pdf->Output('VENTA','I');
 ?>
