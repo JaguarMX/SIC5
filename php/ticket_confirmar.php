47e6de2076c29d02fc3b0c98c13586d30ff69f72
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2022-11-23 10:05:58 -0600

    Update ticket_confirmar.php

--- php/ticket_confirmar.php
+++ php/ticket_confirmar.php
@@ -66,8 +66,8 @@ class PDF extends FPDF{
     $pdf->SetX(6);
     $pdf->SetFont('Helvetica','B', 10);
     $folio = substr(str_repeat(0, 5).$corte, - 6);
-    $pdf->MultiCell(69,4,utf8_decode(date_format(new \DateTime($Info_Corte['fecha'].' '.$Info_Corte['hora']), "d/m/Y H:i" ).'              FOLIO:'.$folio),0,'C',0);
-    $pdf->SetY($pdf->GetY()+2);
+    $pdf->MultiCell(69,4,utf8_decode(date_format(new \DateTime($Info_Corte['fecha'].' '.$Info_Corte['hora']), "d/m/Y H:i" ).'             FOLIO:'.$folio),0,'C',0);
+    $pdf->SetY($pdf->GetY());
     $pdf->SetX(6);
     $pdf->SetFont('Helvetica','', 8);
     $pdf->MultiCell(69,3,utf8_decode('-----------------------------------------------------------------------'),0,'L',0);
