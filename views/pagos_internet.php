87bc38f53263a6e9103a520c9ae4588295ba4dd9
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2022-11-25 14:19:33 -0600

    Update pagos_internet.php

--- views/pagos_internet.php
+++ views/pagos_internet.php
@@ -408,7 +408,7 @@
               <option value="0" >Seleccione Año</option>       
               <option value="2022" selected>2022</option>         
               <option value="2023">2023</option>         
-              <option value="2024">2024</option>         
+              <option value="2024">2024</option>            
             </select>
           </div>
           <div class="col s4 m2 l2">
