a82f32dfeacef8fced2245bd1199f2cbdb4d3223
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2023-01-31 10:05:05 -0600

    reporte credito

--- php/buscar_deudas.php
+++ php/buscar_deudas.php
@@ -3,151 +3,115 @@ include('../php/conexion.php');
 $ValorDe = $conn->real_escape_string($_POST['valorDe']);
 $ValorA = $conn->real_escape_string($_POST['valorA']);
 $Usuario = $conn->real_escape_string($_POST['valorUsuario']);
-$Tipo = $conn->real_escape_string($_POST['valorTipo']);
 
-if ($Usuario != "" AND $Tipo == "") {
+if ($Usuario != 0) {
     $usuario = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM users WHERE user_id = '$Usuario'"));
     $total = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS precio FROM deudas WHERE fecha_deuda>='$ValorDe' AND fecha_deuda<='$ValorA' AND usuario='$Usuario' AND liquidada = 0"));
     $sql_deuda = mysqli_query($conn, "SELECT * FROM deudas WHERE fecha_deuda>='$ValorDe' AND fecha_deuda<='$ValorA' AND usuario='$Usuario' AND liquidada = 0 ORDER BY id_deuda DESC");
     $head = $usuario['firstname'].' '.$usuario['lastname'].':  .  TOTAL = $'.$total['precio'];
-}elseif ($Tipo != "" AND $Usuario == "") {
-    $total = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS precio FROM deudas WHERE fecha_deuda>='$ValorDe' AND fecha_deuda<='$ValorA' AND tipo_cambio='$Tipo' AND liquidada = 0"));
-    $head = $Tipo.':  .  TOTAL = $'.$total['precio'];
-    $sql_deuda = mysqli_query($conn, "SELECT * FROM deudas WHERE fecha_deuda>='$ValorDe' AND fecha_deuda<='$ValorA' AND tipo_cambio = '$Tipo' AND liquidada = 0 ORDER BY id_deuda DESC");
-}else{
-    $total = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS deudas FROM pagos WHERE fecha_deuda>='$ValorDe' AND fecha_deuda<='$ValorA' AND usuario='$Usuario' AND tipo_cambio='$Tipo' AND liquidada = 0"));
-    $head = $Tipo.':  .  TOTAL = $'.$total['precio'];
-    $sql_deuda = mysqli_query($conn, "SELECT * FROM deudas WHERE fecha_deuda>='$ValorDe' AND fecha_deuda<='$ValorA' AND usuario='$Usuario' AND tipo_cambio = '$Tipo' AND liquidada = 0 ORDER BY id_deuda DESC");
-   
+}elseif ($Usuario == 0) {
+    $total = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS precio FROM deudas WHERE fecha_deuda>='$ValorDe' AND fecha_deuda<='$ValorA' AND liquidada = 0"));
+    $head = 'TOTAL = $'.$total['precio'];
+    $sql_deuda = mysqli_query($conn, "SELECT * FROM deudas WHERE fecha_deuda>='$ValorDe' AND fecha_deuda<='$ValorA' AND liquidada = 0 ORDER BY id_deuda DESC");
 }
-
-
 ?>
-
 <div>
-
-<h4 class="blue-text"><?php echo $head;?></h4><br>
+  <h4 class="blue-text"><?php echo $head;?></h4><br>
   <table class="bordered highlight responsive-table">
     <thead>
       <tr>
-        <th>#Cliente</th>
-        <th>Cliente</th>
+        <th>#</th>
+        <th>Estatus</th>
+        <th>Id. Cliente</th>
+        <th>Nombre Cliente</th>
+        <th>Comunidad</th>
+        <th>Descripción</th>    
         <th>Cantidad</th>
         <th>Abono</th>
         <th>Resta</th>
-        <th>Tipo</th>
-        <th>Descripción</th>
         <th>Fecha</th>
-        <?php
-        if ($Usuario != "" AND $Tipo == "") {
-        ?>
-        <th>Cambio</th>
-        <?php
-        }elseif ($Tipo == 'Banco'  OR $Tipo == 'SAN') {
-        ?>
-        <th>Referencia</th>        
-        <th>Registró</th>
-        <?php
-        }else{
-        ?>
-        <th>Registró</th>
-        <?php
-        }
-        ?>
+        <th>Usuario</th>
+        <th>Ver</th>
       </tr>
     </thead>
     <tbody>
-<?php
-$aux = mysqli_num_rows($sql_deuda);
-if($aux>0){
-    $sql = mysqli_query($conn, "SELECT * FROM deudas WHERE liquidada = 0 AND usuario = $Usuario");
-	$filas =  mysqli_num_rows($sql);
-	if ($filas <= 0) {
-	    echo "<center><b><h3>No se encontraron deudas</h3></b></center>";
-	}else{
-	    date_default_timezone_set('America/Mexico_City');
-		$Fecha_Hoy = date('Y-m-d');
-		$cont=0;
-		$total = 0;
-		while ( $resultados = mysqli_fetch_array($sql)) {
-			$id_cliente = $resultados['id_cliente'];
-			$deuda = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS suma FROM deudas WHERE id_cliente = $id_cliente AND liquidada = 1"));
-			$abono = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS suma FROM pagos WHERE id_cliente = $id_cliente AND tipo = 'Abono'"));
-			if ($deuda['suma'] == "") {
-			    $deuda['suma'] = 0;
-			}
-			if ($abono['suma'] == "") {
-				$abono['suma'] = 0;
-			}
-			$poner = mysqli_fetch_array(mysqli_query($conn, "SELECT min(id_deuda) AS id FROM deudas WHERE id_cliente = $id_cliente AND liquidada = 0"));
-			$tiene = $abono['suma']-$deuda['suma'];
-			$cosnulta = mysqli_query($conn,"SELECT * FROM clientes WHERE id_cliente=$id_cliente");
-			if (mysqli_num_rows($cosnulta)<=0) {
-				$cosnulta = mysqli_query($conn,"SELECT * FROM especiales WHERE id_cliente=$id_cliente");
-			} 
-			$cliente = mysqli_fetch_array($cosnulta);
-			$id_comunidad = $cliente['lugar'];
-			$Comunidad = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM comunidades WHERE id_comunidad = $id_comunidad"));	
-			$id_usuario = $resultados['usuario'];
-			$usuario = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM users WHERE user_id = $id_usuario"));
-			$Mas_mes = strtotime('+1 month', strtotime($resultados['fecha_deuda']));
-			$Mas_Mes = date('Y-m-d', $Mas_mes);
-			$color = "green";
-			$estatus = "";
-			if ($Fecha_Hoy >= $Mas_Mes) {
-				$color = "red accent-4";
-				$estatus = "Cobrar";
-			}
-			$cont++;
-			$cantidad = $resultados['cantidad'];
-			if ($cantidad =='') {
-				$cantidad= 0;
-			}
-			if ($poner['id'] == $resultados['id_deuda']) {
-				$tiene = $tiene;
-			}else{
-				$tiene = 0;
-			}
-			$resta = $cantidad-$tiene;
-            ?>
-            <tr>
-                <td><b><?php echo $id_cliente;?></b></td>
-                <td><?php echo $cliente['nombre'];?></td>
-                <td>$<?php echo $resultados['cantidad'];?></td>
-                <td>$<?php echo $tiene; ?></td>
-                <td>$<?php echo $resta; ?></td>
-                <td><?php echo $resultados['tipo'];?></td>
-                <td><?php echo $resultados['descripcion'];?></td>
-                <td><?php echo $resultados['fecha_deuda'];?></td>
-                <?php
-                if ($Usuario != "" AND $Tipo == "") {
-                    ?>
-                    <td><?php echo $resultados['tipo'];?><br><?php if ($resultados['tipo'] == 'Banco' OR $resultados['tipo'] == 'SAN') { echo $refe; } ?></td>
-                    <?php
-                }elseif ($Tipo == 'Banco' OR $Tipo == 'SAN') {
-                    ?>
-                    <td><?php echo $refe;?></td>
-                    <td><?php echo $usuario['firstname'];?></td>
-                    <?php
-                }else{
-                    ?>
-                    <td><?php echo $usuario['firstname'];?></td>
-                    <?php
-                }
-                ?>
-            </tr>
-            <?php
-            $aux--;
-        }
-    }
-}else{
-  echo "<center><b><h5>No hay deudas registradas en esta fecha</h5></b></center>";
-}
-?>
-<?php 
-mysqli_close($conn);
-?>        
+                        <?php
+                        $filas =  mysqli_num_rows($sql_deuda);
+                        if ($filas <= 0) {
+                            echo "<center><b><h3>No se encontraron deudas</h3></b></center>";
+                        }else{
+                            date_default_timezone_set('America/Mexico_City');
+                            $Fecha_Hoy = date('Y-m-d');
+                            $cont=0;
+                            $total = 0;
+                            while ( $resultados = mysqli_fetch_array($sql_deuda)) {
+                                $id_cliente = $resultados['id_cliente'];
+                                $deuda = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS suma FROM deudas WHERE id_cliente = $id_cliente AND liquidada = 1"));
+                                $abono = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS suma FROM pagos WHERE id_cliente = $id_cliente AND tipo = 'Abono'"));
+                                if ($deuda['suma'] == "") {
+                                    $deuda['suma'] = 0;
+                                }
+                                if ($abono['suma'] == "") {
+                                    $abono['suma'] = 0;
+                                }
+                                $poner = mysqli_fetch_array(mysqli_query($conn, "SELECT min(id_deuda) AS id FROM deudas WHERE id_cliente = $id_cliente AND liquidada = 0"));
+                                $tiene = $abono['suma']-$deuda['suma'];
+                                $cosnulta = mysqli_query($conn,"SELECT * FROM clientes WHERE id_cliente=$id_cliente");
+                                if (mysqli_num_rows($cosnulta)<=0) {
+                                    $cosnulta = mysqli_query($conn,"SELECT * FROM especiales WHERE id_cliente=$id_cliente");
+                                } 
+                                $cliente = mysqli_fetch_array($cosnulta);
+                                $id_comunidad = $cliente['lugar'];
+                                $Comunidad = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM comunidades WHERE id_comunidad = $id_comunidad"));   
+                                $id_usuario = $resultados['usuario'];
+                                $usuario = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM users WHERE user_id = $id_usuario"));
+                                $Mas_mes = strtotime('+1 month', strtotime($resultados['fecha_deuda']));
+                                $Mas_Mes = date('Y-m-d', $Mas_mes);
+                                $color = "green";
+                                $estatus = "";
+                                if ($Fecha_Hoy >= $Mas_Mes) {
+                                    $color = "red accent-4";
+                                    $estatus = "Cobrar";
+                                }
+                                $cont++;
+                                $cantidad = $resultados['cantidad'];
+                                if ($cantidad =='') {
+                                    $cantidad= 0;
+                                }
+                                if ($poner['id'] == $resultados['id_deuda']) {
+                                    $tiene = $tiene;
+                                }else{
+                                    $tiene = 0;
+                                }
+                                $resta = $cantidad-$tiene;
+                                ?>
+                                <tr>
+                                    <td><?php echo $cont; ?></td>
+                                    <td><span class="new badge <?php echo$color; ?>" data-badge-caption=""><?php echo $estatus; ?></span></td>
+                                    <td><?php echo $id_cliente; ?></td>
+                                    <td><?php echo $cliente['nombre']; ?></td>
+                                    <td><?php echo $Comunidad['nombre']; ?></td>        
+                                    <td><?php echo $resultados['descripcion']; ?></td>          
+                                    <td>$<?php echo $cantidad; ?></td>                      
+                                    <td>$<?php echo $tiene; ?></td>
+                                    <td>$<?php echo $resta; ?></td>
+                                    <td><?php echo $resultados['fecha_deuda']; ?></td>
+                                    <td><?php echo $usuario['firstname']; ?></td>
+                                <td><form method="post" action="../views/credito.php"><input id="no_cliente" name="no_cliente" type="hidden" value="<?php echo $id_cliente; ?>"><button class="btn-floating btn-tiny waves-effect waves-light pink"><i class="material-icons">send</i></button></form></td>
+                                </tr>
+                                <?php
+                                $total += $resta;
+                            }//FIN WHILE
+                            ?>
+                            <tr>
+                                <td colspan="7">
+                                <td><b>TOTAL:</b></td><td><b> $<?php echo $total; ?></b></td>
+                                <td colspan="4"></td>
+                            </tr>
+                            <?php
+                        }//FIN ELSE
+                        ?>
         </tbody>
-      </table>
-  </div>
-<br>
+    </tbody>
+  </table>
+</div>
--- views/reporte_deudas.php
+++ views/reporte_deudas.php
@@ -4,69 +4,24 @@
 	<title>SIC | Reportes de Deudas</title>
 	<!-- FUNCION PARA BUSCAR -->
 	<script>
-		function buscar_deudas(tipo) {
-		entra = "Si";
-		if (tipo == 1) {
-			var textoDe = $("input#fecha_de2").val();
-			var textoA = $("input#fecha_a2").val();
-			var textoTipo = $("select#tipo").val();
-			var textoUsuario = $("select#usuario2").val();
-			if (textoUsuario == "") {
-			M.toast({html:"Seleccione un usuario.", classes: "rounded"});      
-			entra = "No";    
-			}  
-			if (textoTipo == "") {
-			M.toast({html:"Seleccione un tipo de cambio.", classes: "rounded"});  
-			entra = "No";    
-			}
-		}else if (tipo == 2 ) {
-			var textoDe = $("input#fecha_de3").val();
-			var textoA = $("input#fecha_a3").val();
-			var textoTipo = $("select#tipo3").val();
-			var textoUsuario = $("select#usuario2").val();
-			
-			if (textoTipo == "") {
-			M.toast({html:"Seleccione un tipo de cambio.", classes: "rounded"});  
-			entra = "No";    
-			}
-		}else if (tipo == 0 ){
-			textoTipo = "";
+		function buscar_deudas() {
 			var textoDe = $("input#fecha_de").val();
 			var textoA = $("input#fecha_a").val();
 			var textoUsuario = $("select#usuario").val();
+
 			if (textoUsuario == "") {
-			M.toast({html:"Seleccione un usuario.", classes: "rounded"});      
-			entra = "No";    
-			}    
-		}
-		if (tipo == 3) {
-			entra = "No"; 
-			var textoDe = $("input#fecha_de4").val();
-			var textoA = $("input#fecha_a4").val();
-			if (textoDe == "" || textoA == ""){
-			M.toast({html:"Ingrese un rango de fechas.", classes: "rounded"});
+				M.toast({html:"Seleccione un usuario.", classes: "rounded"});     		
+			} else if (textoDe == "" || textoA == ""){
+				M.toast({html:"Ingrese un rango de fechas.", classes: "rounded"});
 			}else {
-				$.post("../php/buscar_pagos_borrados.php", {
-					valorDe: textoDe,
-					valorA: textoA,
-				}, function(mensaje) {
-					$("#resultado_deudas").html(mensaje);
-				}); 
-			}
-		}else{
-			if (textoDe == "" || textoA == ""){
-			M.toast({html:"Ingrese un rango de fechas.", classes: "rounded"});
-			}else if (entra == "Si") {
 				$.post("../php/buscar_deudas.php", {
 					valorDe: textoDe,
 					valorA: textoA,
 					valorUsuario: textoUsuario,
-					valorTipo: textoTipo
 				}, function(mensaje) {
 					$("#resultado_deudas").html(mensaje);
 				}); 
 			}
-		}
 		
 		};
 	</script>
@@ -85,8 +40,8 @@ include('../php/admin.php');
 		<div class="row">
 			<div class="col s12">
 				<ul id="tabs-swipe-demo" class="tabs">
-					<li class="tab col s3"><a class="active black-text" href="#test-swipe-1">Tabla Deudas</a></li>
-					<li class="tab col s3"><a class="active black-text" href="#test-swipe-2">Filtrar Por Usuarios</a></li>
+					<li class="tab col s6"><a class="active black-text" href="#test-swipe-1">Tabla Deudas</a></li>
+					<li class="tab col s6"><a class="active black-text" href="#test-swipe-2">Filtrar Por Usuarios</a></li>
 				</ul>
 			</div>
       		<br><br><br><br>
@@ -116,78 +71,76 @@ include('../php/admin.php');
 						if ($filas <= 0) {
 							echo "<center><b><h3>No se encontraron deudas</h3></b></center>";
 						}else{
-						date_default_timezone_set('America/Mexico_City');
-						$Fecha_Hoy = date('Y-m-d');
-						$cont=0;
-						$total = 0;
-						while ( $resultados = mysqli_fetch_array($sql)) {
-							$id_cliente = $resultados['id_cliente'];
-							$deuda = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS suma FROM deudas WHERE id_cliente = $id_cliente AND liquidada = 1"));
-							$abono = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS suma FROM pagos WHERE id_cliente = $id_cliente AND tipo = 'Abono'"));
-							if ($deuda['suma'] == "") {
-							$deuda['suma'] = 0;
-							}
-							if ($abono['suma'] == "") {
-							$abono['suma'] = 0;
-							}
-							$poner = mysqli_fetch_array(mysqli_query($conn, "SELECT min(id_deuda) AS id FROM deudas WHERE id_cliente = $id_cliente AND liquidada = 0"));
-							$tiene = $abono['suma']-$deuda['suma'];
-							$cosnulta = mysqli_query($conn,"SELECT * FROM clientes WHERE id_cliente=$id_cliente");
-							if (mysqli_num_rows($cosnulta)<=0) {
-							$cosnulta = mysqli_query($conn,"SELECT * FROM especiales WHERE id_cliente=$id_cliente");
-							} 
-							$cliente = mysqli_fetch_array($cosnulta);
-							$id_comunidad = $cliente['lugar'];
-							$Comunidad = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM comunidades WHERE id_comunidad = $id_comunidad"));	
-							$id_usuario = $resultados['usuario'];
-							$usuario = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM users WHERE user_id = $id_usuario"));
-							$Mas_mes = strtotime('+1 month', strtotime($resultados['fecha_deuda']));
-							$Mas_Mes = date('Y-m-d', $Mas_mes);
-							$color = "green";
-							$estatus = "";
-							if ($Fecha_Hoy >= $Mas_Mes) {
-								$color = "red accent-4";
-								$estatus = "Cobrar";
-							}
-							$cont++;
-							$cantidad = $resultados['cantidad'];
-							if ($cantidad =='') {
-								$cantidad= 0;
-							}
-							if ($poner['id'] == $resultados['id_deuda']) {
-								$tiene = $tiene;
-							}else{
-								$tiene = 0;
-							}
-							$resta = $cantidad-$tiene;
-						?>
-							<tr>
-								<td><?php echo $cont; ?></td>
-								<td><span class="new badge <?php echo$color; ?>" data-badge-caption=""><?php echo $estatus; ?></span></td>
-								<td><?php echo $id_cliente; ?></td>
-								<td><?php echo $cliente['nombre']; ?></td>
-								<td><?php echo $Comunidad['nombre']; ?></td>		
-								<td><?php echo $resultados['descripcion']; ?></td>			
-								<td>$<?php echo $cantidad; ?></td>						
-								<td>$<?php echo $tiene; ?></td>
-								<td>$<?php echo $resta; ?></td>
-								<td><?php echo $resultados['fecha_deuda']; ?></td>
-								<td><?php echo $usuario['firstname']; ?></td>
-							<td><form method="post" action="../views/credito.php"><input id="no_cliente" name="no_cliente" type="hidden" value="<?php echo $id_cliente; ?>"><button class="btn-floating btn-tiny waves-effect waves-light pink"><i class="material-icons">send</i></button></form></td>
-							</tr>
-						<?php
-							$total += $resta;
-						}
+							date_default_timezone_set('America/Mexico_City');
+							$Fecha_Hoy = date('Y-m-d');
+							$cont=0;
+							$total = 0;
+							while ( $resultados = mysqli_fetch_array($sql)) {
+								$id_cliente = $resultados['id_cliente'];
+								$deuda = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS suma FROM deudas WHERE id_cliente = $id_cliente AND liquidada = 1"));
+								$abono = mysqli_fetch_array(mysqli_query($conn, "SELECT SUM(cantidad) AS suma FROM pagos WHERE id_cliente = $id_cliente AND tipo = 'Abono'"));
+								if ($deuda['suma'] == "") {
+									$deuda['suma'] = 0;
+								}
+								if ($abono['suma'] == "") {
+									$abono['suma'] = 0;
+								}
+								$poner = mysqli_fetch_array(mysqli_query($conn, "SELECT min(id_deuda) AS id FROM deudas WHERE id_cliente = $id_cliente AND liquidada = 0"));
+								$tiene = $abono['suma']-$deuda['suma'];
+								$cosnulta = mysqli_query($conn,"SELECT * FROM clientes WHERE id_cliente=$id_cliente");
+								if (mysqli_num_rows($cosnulta)<=0) {
+									$cosnulta = mysqli_query($conn,"SELECT * FROM especiales WHERE id_cliente=$id_cliente");
+								} 
+								$cliente = mysqli_fetch_array($cosnulta);
+								$id_comunidad = $cliente['lugar'];
+								$Comunidad = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM comunidades WHERE id_comunidad = $id_comunidad"));	
+								$id_usuario = $resultados['usuario'];
+								$usuario = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM users WHERE user_id = $id_usuario"));
+								$Mas_mes = strtotime('+1 month', strtotime($resultados['fecha_deuda']));
+								$Mas_Mes = date('Y-m-d', $Mas_mes);
+								$color = "green";
+								$estatus = "";
+								if ($Fecha_Hoy >= $Mas_Mes) {
+									$color = "red accent-4";
+									$estatus = "Cobrar";
+								}
+								$cont++;
+								$cantidad = $resultados['cantidad'];
+								if ($cantidad =='') {
+									$cantidad= 0;
+								}
+								if ($poner['id'] == $resultados['id_deuda']) {
+									$tiene = $tiene;
+								}else{
+									$tiene = 0;
+								}
+								$resta = $cantidad-$tiene;
+								?>
+								<tr>
+									<td><?php echo $cont; ?></td>
+									<td><span class="new badge <?php echo$color; ?>" data-badge-caption=""><?php echo $estatus; ?></span></td>
+									<td><?php echo $id_cliente; ?></td>
+									<td><?php echo $cliente['nombre']; ?></td>
+									<td><?php echo $Comunidad['nombre']; ?></td>		
+									<td><?php echo $resultados['descripcion']; ?></td>			
+									<td>$<?php echo $cantidad; ?></td>						
+									<td>$<?php echo $tiene; ?></td>
+									<td>$<?php echo $resta; ?></td>
+									<td><?php echo $resultados['fecha_deuda']; ?></td>
+									<td><?php echo $usuario['firstname']; ?></td>
+								<td><form method="post" action="../views/credito.php"><input id="no_cliente" name="no_cliente" type="hidden" value="<?php echo $id_cliente; ?>"><button class="btn-floating btn-tiny waves-effect waves-light pink"><i class="material-icons">send</i></button></form></td>
+								</tr>
+								<?php
+								$total += $resta;
+							}//FIN WHILE
 							?>
 							<tr>
-								<td></td><td></td><td></td><td></td><td></td>
-								<td></td>
-								<td></td>
+								<td colspan="7">
 								<td><b>TOTAL:</b></td><td><b> $<?php echo $total; ?></b></td>
-								<td></td><td></td><td></td><td></td>
+								<td colspan="4"></td>
 							</tr>
 							<?php
-						}
+						}//FIN ELSE
 						?>
 					</tbody>				
 				</table><br>
@@ -208,10 +161,11 @@ include('../php/admin.php');
 					<div class="input-field col s12 l4 m4">
 						<select id="usuario" class="browser-default">
 							<option value="" selected>Seleccione un usuario</option>
+							<option value="0">TODOS</option>
 							<?php 
 							$sql_tecnico = mysqli_query($conn,"SELECT * FROM users WHERE estatus = 1");
 							while($tecnico = mysqli_fetch_array($sql_tecnico)){
-							?>
+								?>
 								<option value="<?php echo $tecnico['user_id'];?>"><?php echo $tecnico['user_name'];?></option>
 							<?php
 							}
