364794b7cde13a8934d39ae17bbadea3424c3010
TONYSERMOR19 <130488388+TONYSERMOR19@users.noreply.github.com>
2023-06-28 11:19:11 -0600

    Update create_orden.php
    Se agrego columna a la consulta

--- php/create_orden.php
+++ php/create_orden.php
@@ -92,7 +92,7 @@ if ($Nuevo == 'Si') {
       echo  '<script>M.toast({html:"Ha ocurrido un error.", classes: "rounded"})</script>';  
     }
 
-    ?>
+    ?> 
     <script>
       var a = document.createElement("a");
         a.href = "../views/ordenes_servicio.php";
@@ -125,8 +125,29 @@ if ($Nuevo == 'Si') {
       $sql = "INSERT INTO orden_servicios (id_cliente, ".$es.", fecha, hora, registro, estatus, dpto, precio) VALUES ($IdCliente, '$Solicitud', '$Fecha_hoy', '$Hora', $id_user, '$Estatus', '$Dpto', '$Precio')";
       if(mysqli_query($conn, $sql)){
         echo  '<script>M.toast({html:"Orden de servicio creada.", classes: "rounded"})</script>'; 
-        $ultimo =  mysqli_fetch_array(mysqli_query($conn, "SELECT MAX(id) AS id FROM orden_servicios"));            
+        $ultimo =  mysqli_fetch_array(mysqli_query($conn, "SELECT MAX(id) AS id, id_cliente FROM orden_servicios"));            
         $id = $ultimo['id'];
+
+        if ($anticipo > 0) {
+          $id_cliente   = $ultimo['id_cliente'];
+          $descripcion  = $ultimo['id'];
+          
+          $Fecha_hoy      = date('Y-m-d');
+          $Hora           = date('H:i:s');
+          $tipo_anticipo  = "Abono";
+          
+          $sqlpago = "INSERT INTO pagos (id_cliente, descripcion, cantidad, fecha, hora, tipo, id_user, corte, corteP, tipo_cambio) VALUES ($id_cliente, '$descripcion', '$anticipo', '$Fecha_hoy', '$Hora', '$tipo_anticipo', $id_user, 0, 0, '$tipoPago')";
+			    if(mysqli_query($conn, $sqlpago)){
+            echo  '<script>M.toast({html:"Pago Registrado", classes: "rounded"})</script>';
+            // Si el pago es de banco guardar la referencia....
+            if (($tipoPago == 'Banco' OR $tipoPago == 'SAN') AND $refPago != '') {
+              $ultimoPago =  mysqli_fetch_array(mysqli_query($conn, "SELECT MAX(id_pago) AS id FROM pagos WHERE id_cliente = $id_cliente"));            
+              $id_pago = $ultimoPago['id'];
+              mysqli_query($conn,  "INSERT INTO referencias (id_pago, descripcion, banco) VALUES ('$id_pago', '$refPago', '$banco')");
+            }
+          }
+
+        }
 
         
 
